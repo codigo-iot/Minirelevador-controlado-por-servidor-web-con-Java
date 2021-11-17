a1a09cfcc0962fea40dca3928b6783a49a4373c9
Codigo IoT <83254443+codigo-iot@users.noreply.github.com>
2021-11-17 14:14:30 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+const Gpio = require('onoff').Gpio;
+const led = new Gpio(17, 'out');
+var express = require('express');
+var app = express();
+const path = require('path');
+
+app.get('/',function(req,res){
+    res.sendFile(path.join(__dirname+'/html/index.html'));
+ 
+});
+
+app.get('/encender', function (req, res) {
+        led.writeSync(1);
+        res.sendFile(path.join(__dirname+'/html/index.html'));
+});
+
+app.get('/apagar',function(req,res){
+        led.writeSync(0);
+        res.sendFile(path.join(__dirname+'/html/index.html'));
+});
